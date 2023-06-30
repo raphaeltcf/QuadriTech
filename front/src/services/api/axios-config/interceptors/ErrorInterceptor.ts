26b74b9e1ef4791c2f4e7a98a69679586f3cb536
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-06-30 10:43:14 -0300

    feat: axios and .env configuration

---
+++ front/src/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,11 @@
+import { AxiosError } from 'axios';
+
+export const errorInterceptor = (error: AxiosError) => {
+	if (error.message === 'Network Error') {
+		return Promise.reject(new Error('Erro de Conexão'));
+	}
+	if (error.response?.status === 401) {
+		return Promise.reject(new Error('Usuário não autorizado!'));
+	}
+	return Promise.reject(error);
+};

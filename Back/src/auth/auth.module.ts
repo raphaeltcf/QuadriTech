1c333c2129b0efbcfb2ec0ecfd100e676f8fb247
raphaeltcf <raphaeltcf@gmail.com>
2023-06-30 10:59:43 -0300

    #4-Feat: Start validation with JWT

--- Back/src/auth/auth.module.ts
+++ Back/src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt/dist';
+import 'dotenv/config'
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '3600s' },
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
--- Back/src/auth/auth.service.ts
+++ Back/src/auth/auth.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(private readonly userService: UsersService, private readonly jwtService: JwtService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findUser(username);
+    const isPassawordMatch = await bcrypt.compare(
+      password, 
+      user.password
+    );
+    if (user && isPassawordMatch) {
+      return user;
+    }
+    return null
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  async login(user: any) {
+    const payload = { username: user.username, sub: user._id, roles: user.roles };
+    return {
+      acess_token: this.jwtService.sign(payload),
+    };
+  }  
 }
+
+

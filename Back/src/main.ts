2271b74824f060c34f0061b0a655b1ee3e77eda1
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-07-05 20:38:13 -0300

    feat: adding some dependencies and cors

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  await app.listen(3333);
 }
 bootstrap();
